1c36e5a83b3798b5da2a51bee3e44bd766cb059e
PedDeT <pldelatorre@hotmail.com>
2024-10-20 18:46:06 -0500

    arreglos

--- ejemplo_computadoras.sql
+++ ejemplo_computadoras.sql
@@ -2,8 +2,8 @@ CREATE TABLE computadoras (
     id INT,
     marca CHAR(50),
     modelo CHAR(50),
-    año INT,
+    ano INT,
     precio FLOAT
 );
-INSERT INTO computadoras (id, marca, modelo, año, precio) VALUES (1, 'Dell', 'XPS 13', 2021, 999.99);
-SELECT * FROM computadoras WHERE año >= 2021;
+INSERT INTO computadoras (id, marca, modelo, ano, precio) VALUES (1, 'Dell', 'XPS 13', 2021, 999.99);
+SELECT * FROM computadoras WHERE ano >= 2021;
